ac2639ddbb4b8c446f3049a64932a1dedf3f2c8b
boatengjulius14 <boatengjulius14@gmail.com>
2023-05-15 12:28:17 +0000

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Base Class Module"""
-from datetime import datetime
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
@@ -9,37 +9,40 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """constructor"""
+        from models import storage
         if kwargs:
             for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    self.__dict__[key] = datetime.fromisoformat(value)
-                elif key != "__class__":
-                    self.__dict__[key] = value
+                if key != "__class__":
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
         else:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
         """Returns an informal string representation"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
         """updates the attribute 'update_at' with the current
         datetime
         """
         from models import storage
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__of the instance
+        """updates the attribute 'update_at' with the current
+        datetime
         """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
+        for key, value in self.__dict__.items():
+            if key in ("created_at", "updated_at"):
+                value = self.__dict__[key].isoformat()
+                dictionary[key] = value
         return dictionary
