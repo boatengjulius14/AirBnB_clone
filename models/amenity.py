20723698596a1a27d3cd6ee59957c681cb54e4ce
boatengjulius14 <boatengjulius14@gmail.com>
2023-05-15 17:58:37 +0000

    changes

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines the amenity module"""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """Definition of Amenity class, subclass of BaseModel"""
-
-    name = ""
